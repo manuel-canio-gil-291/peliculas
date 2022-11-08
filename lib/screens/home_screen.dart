7f809cef623deedff3f798d5fe93c04887d31d61
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-08 14:15:42 +0100

    Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Peliculas en cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search_outlined),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Home Screen')
+      )
+    );
   }
 
 }
