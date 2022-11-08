1be8660637764ea419c34c2ee890a49323d6e018
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-08 14:16:46 +0100

    Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/card_swiper.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,8 +17,8 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: Text('Home Screen')
+      body: Column(
+        children: const [CardSwiper()],
       )
     );
   }
