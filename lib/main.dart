7c85d3ba65145e2de29c7ec1e41a98f4668ef776
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-08 14:15:37 +0100

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,14 @@ class MyApp extends StatelessWidget {
       title: 'Peliculas',
       initialRoute: 'home',
       routes: {
-        'home': ( _ ) => HomeScreen(),
-        'details': ( _ ) => DetailsScreen()
+        'home': ( _ ) => const HomeScreen(),
+        'details': ( _ ) => const DetailsScreen()
       },
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Colors.indigo
+        )
+      ),
     );
   }
 }
