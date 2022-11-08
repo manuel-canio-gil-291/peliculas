7d57ff96275ed2d804aa59255b828d808e16bab1
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-08 14:08:14 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/screens/screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: 'home',
+      routes: {
+        'home': ( _ ) => HomeScreen(),
+        'details': ( _ ) => DetailsScreen()
+      },
+    );
+  }
+}
+
