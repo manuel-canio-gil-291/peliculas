56eed34384fb42f24fdf8787282f8588d33404d4
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-10 14:36:19 +0100

    Casting Cards

--- lib/widgets/casting_cards.dart
+++ lib/widgets/casting_cards.dart
@@ -8,7 +8,47 @@ class CastingCards extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 180,
-      color: Colors.red
+      color: Colors.red,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: ( _, int index) => const _CastCard()
+      ),
+    );
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  const _CastCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      width: 110,
+      height: 100,
+      color: Colors.green,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/circular_progress_indicator_square_small.gif'),
+              image: NetworkImage('https://via.placeholder.com/150x300'),
+              height: 140,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            'actor.name someone something',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
