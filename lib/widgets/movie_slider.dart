674ea3c663a19760cdee6701860016c9e6ab384e
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-10 13:16:29 +0100

    Movie Slider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -46,25 +46,31 @@ class _MoviePoster extends StatelessWidget {
       color: Colors.green,
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
-        children: const [
-          FadeInImage(
-            placeholder: AssetImage('assets/no-image.jpg'), 
-            image: NetworkImage('https://via.placeholder.com/300x400'),
-            width: 130,
-            height: 190,
-            fit: BoxFit.cover
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover
+              )
+            )
           ),
-
-          SizedBox(height: 5),
-
-          Text(
+                  
+          const SizedBox(height: 5),
+                  
+          const Text(
             'StarWars: El retorno del nuevo Jedi silvestre de Monte Cristo',
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
             textAlign: TextAlign.center,
-          )
-        ],
-      )
+          ),
+        ]
+      ),
     );
   }
 }
