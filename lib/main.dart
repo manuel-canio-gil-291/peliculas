d73a09a5f511adf2488a7027f890c4fa2aa971cc
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-10 14:07:49 +0100

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Peliculas',
-      initialRoute: 'details',
+      initialRoute: 'home',
       routes: {
         'home': ( _ ) => const HomeScreen(),
         'details': ( _ ) => const DetailsScreen()
