80b36e2a6f07f73fdefbd540f50fa821c2fda416
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-11 13:45:16 +0100

    Movies Provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -7,7 +7,7 @@ class MoviesProvider extends ChangeNotifier {
   
   final String _apiKey = '57b768579acb56e6c5ddb5836c5e31a6';
   final String _baseUrl = 'api.themoviedb.org';
-  final String _language = 'es-ES';
+  final String _language = 'en-US';
   
   MoviesProvider() {
     print('MoviesProvider inicializado');
