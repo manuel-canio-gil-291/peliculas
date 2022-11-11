9dc43a379176bc1012d6004714e9a4e4170cb5e0
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-11 10:50:43 +0100

    Movies Provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert' as convert;
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  
+  final String _apiKey = '57b768579acb56e6c5ddb5836c5e31a6';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
+  
   MoviesProvider() {
     print('MoviesProvider inicializado');
 
@@ -8,6 +16,18 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(
+      _baseUrl,
+      '3/movie/now_playing', {
+      'api_key' : _apiKey,
+      'language' : _language,
+      'page' : '1'
+      }
+    );
+
+    final response = await http.get(url);
+    if(response.statusCode != 200) return print('error');
+    final Map<String, dynamic> decodedData = convert.json.decode(response.body);
+    print(decodedData['dates']);
   }
 }
