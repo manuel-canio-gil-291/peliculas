64056e2ce73fd7779971ea01d2ec72986e915ad8
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-11 14:44:44 +0100

    Home Screen Fail

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Peliculas en cines'),
@@ -18,9 +23,9 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: Column(
-        children: const [
+        children: [
           //Tarjetas principales
-          CardSwiper(),
+          CardSwiper(movies: moviesProvider.onDisplayMovies),
           
           //Slider de peliculas
           MovieSlider(),
