9e0bca9a3132a4b30b1b9850b35c943bdec84377
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-15 14:36:28 +0100

    Movie

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,48 +1,120 @@
 
+//class Movie {
+//    Movie({
+//        required this.adult,
+//        this.backdropPath,
+//        required this.genreIds,
+//        required this.id,
+//        this.originalLanguage,
+//        required this.originalTitle,
+//        required this.overview,
+//        required this.popularity,
+//        this.posterPath,
+//        this.releaseDate,
+//        required this.title,
+//        required this.video,
+//        required this.voteAverage,
+//        required this.voteCount,
+//    });
+//
+//    bool adult;
+//    String? backdropPath;
+//    List<int> genreIds;
+//    int id;
+//    String? originalLanguage;
+//    String originalTitle;
+//    String overview;
+//    double popularity;
+//    String? posterPath;
+//    String? releaseDate;
+//    String title;
+//    bool video;
+//    double voteAverage;
+//    int voteCount;
+//
+//    factory Movie.fromJson(Map<String, dynamic> json) => Movie(
+//        adult: json["adult"],
+//        backdropPath: json["backdrop_path"],
+//        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+//        id: json["id"],
+//        originalLanguage: json["original_language"],
+//        originalTitle: json["original_title"],
+//        overview: json["overview"],
+//        popularity: json["popularity"].toDouble(),
+//        posterPath: json["poster_path"],
+//        releaseDate: json["release_date"],
+//        title: json["title"],
+//        video: json["video"],
+//        voteAverage: json["vote_average"].toDouble(),
+//        voteCount: json["vote_count"],
+//    );
+//
+//    Map<String, dynamic> toMap() => {
+//        "adult": adult,
+//        "backdrop_path": backdropPath,
+//        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
+//        "id": id,
+//        "original_language": originalLanguage,
+//        "original_title": originalTitle,
+//        "overview": overview,
+//        "popularity": popularity,
+//        "poster_path": posterPath,
+//        "release_date": releaseDate,
+//        "title": title,
+//        "video": video,
+//        "vote_average": voteAverage,
+//        "vote_count": voteCount,
+//    };
+//}
+//
+//enum OriginalLanguage { EN, DE, JA, ES }
+
+import 'package:peliculas/models/models.dart';
+
 class Movie {
     Movie({
         required this.adult,
-        this.backdropPath,
+        required this.backdropPath,
         required this.genreIds,
         required this.id,
         this.originalLanguage,
-        required this.originalTitle,
+        this.originalTitle,
         required this.overview,
         required this.popularity,
-        this.posterPath,
-        this.releaseDate,
+        required this.posterPath,
+        required this.releaseDate,
         required this.title,
         required this.video,
-        required this.voteAverage,
+        this.voteAverage,
         required this.voteCount,
     });
 
     bool adult;
-    String? backdropPath;
+    String backdropPath;
     List<int> genreIds;
     int id;
-    String? originalLanguage;
-    String originalTitle;
+    OriginalLanguage? originalLanguage;
+    String? originalTitle;
     String overview;
     double popularity;
-    String? posterPath;
-    String? releaseDate;
+    String posterPath;
+    DateTime releaseDate;
     String title;
     bool video;
-    double voteAverage;
+    double? voteAverage;
     int voteCount;
 
-    factory Movie.fromJson(Map<String, dynamic> json) => Movie(
+    factory Movie.fromMap(Map<String, dynamic> json) => Movie(
         adult: json["adult"],
         backdropPath: json["backdrop_path"],
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
         id: json["id"],
-        originalLanguage: json["original_language"],
+        originalLanguage: originalLanguageValues.map[json["original_language"]],
         originalTitle: json["original_title"],
         overview: json["overview"],
         popularity: json["popularity"].toDouble(),
         posterPath: json["poster_path"],
-        releaseDate: json["release_date"],
+        releaseDate: DateTime.parse(json["release_date"]),
         title: json["title"],
         video: json["video"],
         voteAverage: json["vote_average"].toDouble(),
@@ -54,12 +126,12 @@ class Movie {
         "backdrop_path": backdropPath,
         "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
         "id": id,
-        "original_language": originalLanguage,
+        "original_language": originalLanguageValues.reverse[originalLanguage],
         "original_title": originalTitle,
         "overview": overview,
         "popularity": popularity,
         "poster_path": posterPath,
-        "release_date": releaseDate,
+        "release_date": "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
         "title": title,
         "video": video,
         "vote_average": voteAverage,
@@ -68,3 +140,10 @@ class Movie {
 }
 
 enum OriginalLanguage { EN, DE, JA, ES }
+
+final originalLanguageValues = EnumValues({
+    "de": OriginalLanguage.DE,
+    "en": OriginalLanguage.EN,
+    "es": OriginalLanguage.ES,
+    "ja": OriginalLanguage.JA
+});
