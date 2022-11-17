a0380bc8369792073f0171edd2dd76fd8fd1b261
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-17 13:47:42 +0100

    Popular Response

--- lib/models/popular_response.dart
+++ lib/models/popular_response.dart
@@ -3,6 +3,8 @@
 //     final popularResponse = popularResponseFromMap(jsonString);
 
 import 'dart:convert';
+import 'movie.dart';
+
 
 class PopularResponse {
     PopularResponse({
@@ -13,7 +15,7 @@ class PopularResponse {
     });
 
     int page;
-    List<Result> results;
+    List<Movie> results;
     int totalPages;
     int totalResults;
 
@@ -23,7 +25,7 @@ class PopularResponse {
 
     factory PopularResponse.fromMap(Map<String, dynamic> json) => PopularResponse(
         page: json["page"],
-        results: List<Result>.from(json["results"].map((x) => Result.fromMap(x))),
+        results: List<Movie>.from(json["results"].map((x) => Movie.fromMap(x))),
         totalPages: json["total_pages"],
         totalResults: json["total_results"],
     );
@@ -33,77 +35,5 @@ class PopularResponse {
         "results": List<dynamic>.from(results.map((x) => x.toMap())),
         "total_pages": totalPages,
         "total_results": totalResults,
-    };
-}
-
-class Result {
-    Result({
-        required this.adult,
-        required this.backdropPath,
-        required this.genreIds,
-        required this.id,
-        required this.originalLanguage,
-        required this.originalTitle,
-        required this.overview,
-        required this.popularity,
-        required this.posterPath,
-        required this.releaseDate,
-        required this.title,
-        required this.video,
-        required this.voteAverage,
-        required this.voteCount,
-    });
-
-    bool adult;
-    String backdropPath;
-    List<int> genreIds;
-    int id;
-    String originalLanguage;
-    String originalTitle;
-    String overview;
-    double popularity;
-    String posterPath;
-    DateTime releaseDate;
-    String title;
-    bool video;
-    double voteAverage;
-    int voteCount;
-
-    factory Result.fromJson(String str) => Result.fromMap(json.decode(str));
-
-    String toJson() => json.encode(toMap());
-
-    factory Result.fromMap(Map<String, dynamic> json) => Result(
-        adult: json["adult"],
-        backdropPath: json["backdrop_path"],
-        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
-        id: json["id"],
-        originalLanguage: json["original_language"],
-        originalTitle: json["original_title"],
-        overview: json["overview"],
-        popularity: json["popularity"].toDouble(),
-        posterPath: json["poster_path"],
-        releaseDate: DateTime.parse(json["release_date"]),
-        title: json["title"],
-        video: json["video"],
-        voteAverage: json["vote_average"].toDouble(),
-        voteCount: json["vote_count"],
-    );
-
-    Map<String, dynamic> toMap() => {
-        "adult": adult,
-        "backdrop_path": backdropPath,
-        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
-        "id": id,
-        "original_language": originalLanguage,
-        "original_title": originalTitle,
-        "overview": overview,
-        "popularity": popularity,
-        "poster_path": posterPath,
-        "release_date": "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
-        "title": title,
-        "video": video,
-        "vote_average": voteAverage,
-        "vote_count": voteCount,
     };
 }
