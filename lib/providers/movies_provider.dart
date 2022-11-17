b8d0f95f9f495524cacaaf073c569ff7130925b3
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-17 13:51:12 +0100

    Movies Provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:peliculas/models/models.dart';
+import 'package:peliculas/models/popular_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   
@@ -21,7 +22,7 @@ class MoviesProvider extends ChangeNotifier {
   getOnDisplayMovies() async {
     var url = Uri.https(
       _baseUrl,
-      '3/movie/now_playing', {
+      '3/movie/popular', {
       'api_key' : _apiKey,
       'language' : _language,
       'page' : '1'
@@ -31,6 +32,27 @@ class MoviesProvider extends ChangeNotifier {
     final response = await http.get(url);
     if(response.statusCode != 200) return print('error');
     final nowPlayingResponse = NowPlayingResponse.fromMap(json.decode(response.body));
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    
+    onDisplayMovies = nowPlayingResponse.results;
+
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    var url = Uri.https(
+      _baseUrl,
+      '3/movie/now_playing', {
+      'api_key' : _apiKey,
+      'language' : _language,
+      'page' : '1'
+      }
+    );
+
+    final response = await http.get(url);
+    if(response.statusCode != 200) return print('error');
+    final nowPlayingResponse = PopularResponse.fromMap(json.decode(response.body));
 
     final Map<String, dynamic> decodedData = json.decode(response.body);
     
