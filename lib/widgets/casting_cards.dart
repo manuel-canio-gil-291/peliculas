ab74cadc046fa88e016b0b1732f520553008d3b4
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-18 14:13:10 +0100

    Casting Cards

--- lib/widgets/casting_cards.dart
+++ lib/widgets/casting_cards.dart
@@ -18,18 +18,21 @@ class CastingCards extends StatelessWidget {
       builder: ( _ , AsyncSnapshot<List<Cast>> snapshot) {
         if(!snapshot.hasData) {
           return Container(
-            
-
+            constraints: const BoxConstraints(maxWidth: 150),
+            height: 180,
+            child: const CircularProgressIndicator(),
           );
         }
+
+          final List<Cast> cast = snapshot.data!;
           return Container(
           width: double.infinity,
           height: 180,
           color: Colors.white,
           child: ListView.builder(
-            itemCount: 10,
+            itemCount: cast.length,
             scrollDirection: Axis.horizontal,
-            itemBuilder: ( _, int index) => const _CastCard()
+            itemBuilder: ( _, int index) => _CastCard(actor: cast[index])
           ),
         );
       
@@ -41,7 +44,10 @@ class CastingCards extends StatelessWidget {
 }
 
 class _CastCard extends StatelessWidget {
-  const _CastCard({Key? key}) : super(key: key);
+
+  final Cast actor;
+
+  const _CastCard({Key? key, required this.actor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -54,17 +60,17 @@ class _CastCard extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(20),
-            child: const FadeInImage(
-              placeholder: AssetImage('assets/circular_progress_indicator_square_small.gif'),
-              image: NetworkImage('https://via.placeholder.com/150x300.jpg'),
+            child: FadeInImage(
+              placeholder: const AssetImage('assets/circular_progress_indicator_square_small.gif'),
+              image: NetworkImage(actor.fullProfilePath),
               height: 140,
               width: 100,
               fit: BoxFit.cover,
             ),
           ),
           const SizedBox(height: 5),
-          const Text(
-            'actor.name someone something',
+          Text(
+            actor.name,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
             textAlign: TextAlign.center,
