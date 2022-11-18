001a100389797d7a4f1edd836f2e59fb123a2869
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-11-18 14:45:51 +0100

    Card Swiper

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_card_swipper/flutter_card_swiper.dart';
 import 'package:peliculas/models/models.dart';
 
-class CardSwiper extends StatelessWidget {
+class CardSwiper extends StatefulWidget {
   const CardSwiper(List<Movie> onDisplayMovies, {super.key, required this.movies});
 
   final List<Movie> movies;
 
   @override
+  State<CardSwiper> createState() => _CardSwiperState();
+}
+
+class _CardSwiperState extends State<CardSwiper> {
+    
+  @override
   Widget build(BuildContext context) {
 
     final size = MediaQuery.of(context).size;
-    if(movies.isEmpty){
+    if(widget.movies.isEmpty){
       return SizedBox(
       width: double.infinity,
       height: size.height * 0.4,
@@ -25,13 +31,13 @@ class CardSwiper extends StatelessWidget {
       height: size.height * 0.5,
       color: Colors.white,
       child: Swiper(
-        itemCount: movies.length, 
+        itemCount: widget.movies.length, 
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.height * 0.4,
         itemBuilder: ( _, int index) {
 
-          final movie = movies[index];
+          final movie = widget.movies[index];
           return GestureDetector(
             onTap: () => Navigator.pushNamed(context, 'details'),
             child: ClipRRect(
